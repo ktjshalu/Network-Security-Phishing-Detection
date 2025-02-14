38282df02e277d7ee3ce90627b02ce870f060a24
kjshalu <kjshalu670@gmail.com>
2025-02-14 11:57:52 +0530

    Project structure set up

---
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -0,0 +1,60 @@
+import os
+import sys
+import numpy as np
+import pandas as pd
+
+"""
+defining common constant variable for training pipeline
+"""
+TARGET_COLUMN = "Result"
+PIPELINE_NAME: str = "NetworkSecurity"
+ARITFACT_DIR: str = "Artifacts"
+FILE_NAME: str = "phisingData.csv"
+
+TRAIN_FILE_NAME: str = "train.csv"
+TEST_FILE_NAME: str = "test.csv"
+
+SCHEMA_FILE_PATH =os.path.join("data_schema","schema.yaml")
+
+"""
+Data Ingestion related constant start with DATA_INGESTION VAR NAME
+"""
+DATA_INGESTION_COLLECTION_NAME: str ="NetworkData"
+DATA_INGESTION_DATABASE_NAME: str ="SHALUAI"
+DATA_INGESTION_DIR_NAME: str ="data_ingestion"
+DATA_INGESTION_FEATURE_STORE_DIR:str ="feature_store"
+DATA_INGESTION_INGESTED_DIR_NAME:str ="ingested"
+DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: float = 0.2
+
+
+"""
+Data Validation related start with DATA_VALIDATION VAR NAME
+"""
+
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+DATA_VALIDATION_VALID_DIR_NAME: str = "validated"
+DATA_VALIDATION_INVALID_DIR_NAME: str = "invalid"
+DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
+PREPROCESSING_OBJECT_FILE_NAME = "preprocessing.pkl"
+
+
+"""
+Data Transformation related constant start with DATA_TRANSFORMATION VAR NAME
+"""
+
+DATA_TRANSFROMATION_DIR_NAME: str="data_transformation"
+DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR: str="transformed"
+DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR: str="transformed_object"
+
+"""
+## kkn imputer to replace nan values
+"""
+DATA_TRANSFORMATION_IMPUTER_PARAMS: dict={
+    "missing_values": np.nan,
+    "n_neighbors": 3,
+    "weights": "uniform"
+}
+
+DATA_TRANSFORMATION_TRAIN_FILE_PATH: str= "train.npy"
+DATA_TRANSFORMATION_TEST_FILE_PATH: str= "test.npy"
